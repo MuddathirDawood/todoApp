f5e34dbf4e5e0af4fa8b4bc20de88cbdab78af29
MuddathirDawood <dmuddathir@gmail.com>
2022-06-24 14:06:31 +0200

    buggy

--- script/code.js
+++ script/code.js
@@ -10,6 +10,16 @@ JSON.parse(localStorage.getItem('records')) : [
 document.addEventListener("DOMContentLoaded", ()=> {showData();});
     const addItems = document.querySelector('#add');
 
+items.forEach((item,index)=>{
+        document.querySelector('#content').innerHTML += `
+        <div class="items">
+        <div><input type="checkbox" id="check" onclick="visible(${index})"></div>
+        <div id="name">${item.item}</div>
+        <div><i class="bi bi-x" id="remove" onclick="removeItem(${index})"></i></div>
+        </div>
+        `
+    })
+
 function showData(){
     addItems.addEventListener('click',()=>{
         document.querySelector('#content').innerHTML = '';
